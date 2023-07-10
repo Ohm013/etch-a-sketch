9daa54171a7d127c960f0dbb43b024cc2bf6f3d8
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-07-10 10:35:59 -0500

    make click event listener to style background black, also linked it to another function with eventlistener to style background black when hovered over

--- javascript.js
+++ javascript.js
@@ -7,8 +7,22 @@ function makeDivs (numDivs) {
     div.classList.add('squares'); 
     div.style.cssText = "border: 1px solid black ; height: 30px ; width: 30px ;"; 
     container.appendChild(div);
+    div.addEventListener('click', function(e)  {
+        e.stopPropagation() ; 
+        e.target.style.background = 'black';
+        startColor() ;
+    })  //prob gonna have to change the event type to something else instead of 'click' to make boxes become black when hovering over
+     
+    };
 } 
 
-}
 makeDivs(256) ; 
 
+
+function startColor () {
+    addEventListener('mouseover', function(e)  {
+        
+        e.target.style.background = 'black';
+
+    } )
+}
