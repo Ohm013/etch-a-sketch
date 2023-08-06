8a292f011aef147e94d066160ba305f0630091ef
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-08-06 14:24:01 -0500

    added 'container.style.background =white so the grid doesnt color the background of the container. Without it, it would leave the container background the color chosen when change grid size or colors. Updated the old variable names from cubes to columns and sqaures to rows and also updated issue and solution list

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // Issue #1 : When coloring, the marker would color the div squares, the whole container, and if mouse on the page it would make the whole whole page black
 
-// Solution #1: Restricted the mouseover event to only container and using target.matches >> container.addEventListener('mouseover', function(e) { 
+// Solution #1: Restricted the mouseover event to only container and  using target.matches >> container.addEventListener('mouseover', function(e) { 
   //target = e.target ;
 
    // if (target.matches = "div.squares") 
@@ -10,7 +10,16 @@
 
 // Solution #2 Added >>> container.textContent = ""; <<< to update the grid box to blank every time the function is called.
 
-// Issue #3: When resizing grid, it resizes improperly. Leaves a few squares on the last row by themselves. Tried "box-sizing : border-box"in CSS and that solved some of the grids but not all bc border-box means it takes into account the border of each cube when making the size of it
+//**/ Issue #3 **: When resizing grid, it resizes improperly. Leaves a few squares on the last row by themselves. Tried "box-sizing : border-box"in CSS and that solved some of the grids but not all bc border-box means it takes into account the border of each cube when making the size of it
+// ** Solution #3 ** : Did nested loops, one for rows and inside loop to make squares within the row (basicaly columns). Added "flex: 1 1 auto" to the rows and columns in CSS. the flex: 1 1 auto was huge and changing flex direction to column for the container. flex: 1 1 auto allows the insinde squares to grow or shrink as needed to fit the container
+
+
+// Issue #4 : The container background would remain the color it was previously colored when grid sizing and changing colors
+// Solution #4 : added "container.style.background = "white";" when switchin between colors and grid resize 
+
+//Issue #5: When coloring, if mouse hovers over the side borders it colors the whole row the chosen color but doesn't do it when I clear and begin coloring again. Only when page is refreshed at the first 16 x 16. How to not make it color whole row?
+// Solution #5 : 
+
 
 const container = document.querySelector('#container');
 const rainbow = document.querySelector('#rainbow');
@@ -19,37 +28,21 @@ const clear = document.querySelector("#clear");
 const black = document.querySelector("#black"); 
 const eraser = document.querySelector("#eraser");
 
+//try two loops(nested loop) for gridSize function
 
-//make function to start game off with 16x16 which then can be changed with the button
-//function defaultGrid (size){
-//size = 256
-//for (i = 0; i < 256  ; i++ ){
- // let dimension = Math.floor((Math.sqrt(360000 / 256)));
-  //const div = document.createElement('div'); 
-  //div.classList.add('squares'); 
-  //div.style.cssText = `border: .3px solid black; width: ${dimension}px ; height ${dimension}px`; 
-  //container.appendChild(div);
-//}};
-
-
-
-let rows = document.createElement('div');
-let columns = document.createElement('div');
-
-function newGrid () {
+function newGrid (size) {
         white(); 
-        container.textContent = ""; //resets the container to blank 
-        let question = prompt("How many squares on each side? (Enter only one number) "); 
-          if (question > 100) {
+        container.textContent = ""; //resets the container to blank , no lines from previous grid
+          if ( size > 100) {
             alert ("Number must be 100 or less"); 
-        } else if (question <= 100) {
-          for (let i = 0 ; i < question ; i++) {
+        } else if (size <= 100) {
+          for (let i = 0 ; i < size ; i++) {
             rows = document.createElement('div');
             rows.classList.add('rows'); 
             rows.style.cssText = `border: .1px solid black`;
             container.appendChild(rows); 
             
-            for (let j = 0 ; j < question; j++){
+            for (let j = 0 ; j < size; j++){
               columns = document.createElement('div');
               columns.classList.add('columns'); 
               columns.style.cssText =  `border: .1px solid black`; 
@@ -58,8 +51,14 @@ function newGrid () {
        };
    };
 };
-  
-gridSize.addEventListener('click', newGrid);
+
+newGrid(16);
+
+
+gridSize.addEventListener('click', () => {
+  size = prompt("How many squares on each side? (Enter only one number) "); 
+  newGrid(size) ;
+});
 
 eraser.addEventListener('click', erase);
 
@@ -72,12 +71,13 @@ function erase () {
 
 clear.addEventListener('click', white);
 
+
 function white () { //resets all squares to white when clear is clicked
-    const squares = document.querySelectorAll('.rows') ;
-    const cubes = document.querySelectorAll('squares'); 
-    cubes.forEach(cube => cube.style.background = "white");
-    squares.forEach(square => square.style.background = "white");
-    container.style.cssText = "background : white" ; 
+    container.style.background = "white"; 
+    const rows = document.querySelectorAll('.rows') ;
+    const columns = document.querySelectorAll('.columns'); 
+    rows.forEach(row => row.style.background = "white");
+    columns.forEach(column => column.style.background = "white");
 }
 
 
@@ -85,7 +85,8 @@ black.addEventListener('click', dark) ;
 
 function changeColor (target) {
   target.style.backgroundColor = "black" ; 
-}
+  container.style.background = "white"; 
+};
 
 function dark () {
   container.addEventListener('mouseover', function(e) {
@@ -107,6 +108,37 @@ function beginrgb() {
         let z = Math.floor(Math.random() *256);
         var rgbColor = `rgb(${x},${y}, ${z})`; 
         e.target.style.background = rgbColor; 
+        container.style.background = "white"; 
     }});
  };
   
+
+// The first function was the code for default 16 x 16 when game is first loaded
+
+//make function to start game off with 16x16 which then can be changed with the button
+//function defaultGrid (size){
+//size = 256
+//for (i = 0; i < 256  ; i++ ){
+ // let dimension = Math.floor((Math.sqrt(360000 / 256)));
+ // const div = document.createElement('div'); 
+  //div.classList.add('squares'); 
+  //div.style.cssText = `border: .3px solid black; width: ${dimension}px ; height ${dimension}px`; 
+  //container.appendChild(div);
+//}};
+
+//defaultGrid() ; 
+
+
+//  This was attemped grid resize function that didnt work because it would leave extra space or put a couple squares on a new row by itself. Wouldnt fill container properly. Box-border worked to an extenet but not fully
+
+//let numDivs = (question ** 2) ; 
+//let eachSquare = 360000 / (numDivs) ;   // area of each square
+//let wL = Math.floor((Math.sqrt(eachSquare)));  // gets width & length of each square
+//for (let i = 0; i < numDivs ; i++ ){
+//numDivs = question ** 2; 
+//const div = document.createElement('div'); 
+//div.classList.add('squares'); 
+//div.style.cssText = `border: .1px solid black; width: ${wL}px ; height ${wL}px`; 
+//container.appendChild(div)
+//}
+//});
